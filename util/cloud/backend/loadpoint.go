0494dbf497117e12f0cc1bada0ef35a6f1c8203d
andig <cpuidle@gmx.de>
2021-11-29 18:32:48 +0100

    wip

---
+++ util/cloud/backend/loadpoint.go
@@ -0,0 +1,200 @@
+package backend
+
+import (
+	"time"
+
+	"github.com/evcc-io/evcc/api"
+	"github.com/evcc-io/evcc/api/proto/pb"
+	"github.com/evcc-io/evcc/core/loadpoint"
+	"github.com/evcc-io/evcc/util"
+	"github.com/evcc-io/evcc/util/cloud"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type Adapter struct {
+	log    *util.Logger
+	sender Sender
+	ID     int32
+}
+
+type Sender interface {
+	Execute(*pb.EdgeRequest) (*pb.EdgeResponse, error)
+}
+
+var _ loadpoint.API = (*Adapter)(nil)
+
+func NewAdapter(log *util.Logger, sender Sender, id int32) *Adapter {
+	return &Adapter{
+		log:    log,
+		sender: sender,
+		ID:     id,
+	}
+}
+
+func (lp *Adapter) send(api cloud.ApiCall, req *pb.EdgeRequest) (*pb.EdgeResponse, error) {
+	if req == nil {
+		req = &pb.EdgeRequest{}
+	}
+
+	req.Loadpoint = lp.ID + 1
+	resp, err := lp.sender.Execute(req)
+	if err != nil {
+		lp.log.ERROR.Printf("calling %d: %v", api, err)
+	}
+
+	return resp, err
+}
+
+func (lp *Adapter) Name() string {
+	resp, err := lp.send(cloud.Name, nil)
+	if err != nil {
+		return ""
+	}
+	return resp.Stringval
+}
+
+func (lp *Adapter) HasChargeMeter() bool {
+	resp, err := lp.send(cloud.HasChargeMeter, nil)
+	if err != nil {
+		return false
+	}
+	return resp.Boolval
+
+}
+
+func (lp *Adapter) GetStatus() api.ChargeStatus {
+	resp, err := lp.send(cloud.GetStatus, nil)
+	if err != nil {
+		return ""
+	}
+	return api.ChargeStatus(resp.Stringval)
+}
+
+func (lp *Adapter) GetMode() api.ChargeMode {
+	resp, err := lp.send(cloud.GetMode, nil)
+	if err != nil {
+		return ""
+	}
+	return api.ChargeMode(resp.Stringval)
+}
+
+func (lp *Adapter) SetMode(val api.ChargeMode) {
+	_, err := lp.send(cloud.RemoteControl, &pb.EdgeRequest{Stringval: string(val)})
+	_ = err
+}
+
+func (lp *Adapter) GetTargetSoC() int {
+	resp, err := lp.send(cloud.GetTargetSoC, nil)
+	if err != nil {
+		return 0
+	}
+	return int(resp.Intval)
+}
+
+func (lp *Adapter) SetTargetSoC(val int) error {
+	_, err := lp.send(cloud.SetTargetSoC, &pb.EdgeRequest{Intval: int64(val)})
+	return err
+}
+
+func (lp *Adapter) GetMinSoC() int {
+	resp, err := lp.send(cloud.GetMinSoC, nil)
+	if err != nil {
+		return 0
+	}
+	return int(resp.Intval)
+}
+
+func (lp *Adapter) SetMinSoC(val int) error {
+	_, err := lp.send(cloud.SetMinSoC, &pb.EdgeRequest{Intval: int64(val)})
+	_ = err
+	return err
+}
+
+func (lp *Adapter) GetPhases() int {
+	resp, err := lp.send(cloud.GetPhases, nil)
+	if err != nil {
+		return 0
+	}
+	return int(resp.Intval)
+}
+
+func (lp *Adapter) SetPhases(val int) error {
+	_, err := lp.send(cloud.SetPhases, &pb.EdgeRequest{Intval: int64(val)})
+	return err
+}
+
+func (lp *Adapter) SetTargetCharge(t time.Time, val int) {
+	_, err := lp.send(cloud.SetTargetCharge, &pb.EdgeRequest{Timeval: timestamppb.New(t), Intval: int64(val)})
+	_ = err
+}
+
+func (lp *Adapter) GetChargePower() float64 {
+	resp, err := lp.send(cloud.GetChargePower, nil)
+	if err != nil {
+		return 0
+	}
+	return resp.Floatval
+}
+
+func (lp *Adapter) GetMinCurrent() float64 {
+	resp, err := lp.send(cloud.GetMinCurrent, nil)
+	if err != nil {
+		return 0
+	}
+	return resp.Floatval
+}
+
+func (lp *Adapter) SetMinCurrent(val float64) {
+	_, err := lp.send(cloud.SetMinCurrent, &pb.EdgeRequest{Floatval: val})
+	_ = err
+}
+
+func (lp *Adapter) GetMaxCurrent() float64 {
+	resp, err := lp.send(cloud.GetMaxCurrent, nil)
+	if err != nil {
+		return 0
+	}
+	return resp.Floatval
+}
+
+func (lp *Adapter) SetMaxCurrent(val float64) {
+	_, err := lp.send(cloud.SetMaxCurrent, &pb.EdgeRequest{Floatval: val})
+	_ = err
+}
+
+func (lp *Adapter) GetMinPower() float64 {
+	resp, err := lp.send(cloud.GetMinPower, nil)
+	if err != nil {
+		return 0
+	}
+	return resp.Floatval
+}
+
+func (lp *Adapter) GetMaxPower() float64 {
+	resp, err := lp.send(cloud.GetMaxPower, nil)
+	if err != nil {
+		return 0
+	}
+	return resp.Floatval
+}
+
+func (lp *Adapter) GetRemainingDuration() time.Duration {
+	resp, err := lp.send(cloud.GetRemainingDuration, nil)
+	if err != nil {
+		return 0
+	}
+	return resp.Durationval.AsDuration()
+}
+
+func (lp *Adapter) GetRemainingEnergy() float64 {
+	resp, err := lp.send(cloud.GetRemainingEnergy, nil)
+	if err != nil {
+		return 0
+	}
+	return resp.Floatval
+}
+
+func (lp *Adapter) RemoteControl(_ string, demand loadpoint.RemoteDemand) {
+	_, err := lp.send(cloud.RemoteControl, &pb.EdgeRequest{Stringval: string(demand)})
+	_ = err
+}
---
+++ util/cloud/backend/server.go
@@ -0,0 +1,60 @@
+package backend
+
+import (
+	"bytes"
+	"encoding/gob"
+
+	"github.com/evcc-io/evcc/api/proto/pb"
+	"github.com/evcc-io/evcc/util"
+)
+
+type Server struct {
+	log           *util.Logger
+	UpdateHandler func(util.Param)
+	pb.UnimplementedCloudConnectServiceServer
+}
+
+func (s *Server) SubscribeEdgeRequest(req *pb.EdgeEnvironment, srv pb.CloudConnectService_SubscribeEdgeRequestServer) error {
+	// handle edge clients connecting
+	return nil
+}
+
+func (s *Server) SendEdgeResponse(inS pb.CloudConnectService_SendEdgeResponseServer) error {
+	for {
+		req, err := inS.Recv()
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func ParamFromUpdateRequest(req *pb.UpdateRequest) (util.Param, error) {
+	param := util.Param{
+		Key: req.Key,
+	}
+
+	if req.Loadpoint > 0 {
+		i := int(req.Loadpoint - 1)
+		param.LoadPoint = &i
+	}
+
+	err := gob.NewDecoder(bytes.NewReader(req.Val)).Decode(&param.Val)
+
+	return param, err
+}
+
+func (s *Server) SendEdgeUpdate(inS pb.CloudConnectService_SendEdgeUpdateServer) error {
+	for {
+		req, err := inS.Recv()
+		if err != nil {
+			return err
+		}
+
+		param, err := ParamFromUpdateRequest(req)
+		if err != nil {
+			s.log.ERROR.Printf("failed to decode update request: %v", err)
+		} else {
+			s.UpdateHandler(param)
+		}
+	}
+}
