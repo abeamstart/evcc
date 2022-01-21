ab1ce0abb49fb288bd7ac56f0a8c95d232499751
andig <cpuidle@gmx.de>
2022-01-21 10:27:43 +0100

    Simplify

--- vehicle/ford/api.go
+++ vehicle/ford/api.go
@@ -103,12 +103,6 @@ func (v *API) RefreshRequest(vin string) (string, error) {
 // WakeUp performs a wakeup request
 func (v *API) WakeUp(vin string) error {
 	uri := fmt.Sprintf("%s/api/dashboard/v1/users/vehicles?wakeupVin=%s", TokenURI, vin)
-
-	res, err := v.GetBody(uri)
-
-	if err == nil && res == nil {
-		err = errors.New("wakeup failed")
-	}
-
+	_, err := v.GetBody(uri)
 	return err
 }
